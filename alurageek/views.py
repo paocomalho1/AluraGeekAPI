7f9fb4015c2221eefc576cde5d6ab4a9b11c6b92
ERICK <erick_honorio20@hotmail.com>
2022-10-12 17:02:21 -0300

    Autenticacao produtos

--- alurageek/views.py
+++ alurageek/views.py
@@ -2,6 +2,8 @@ from re import search
 from rest_framework import viewsets
 from alurageek.models import Produtos
 from alurageek.serializers import ProdutosSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class ProdutoViewSet(viewsets.ModelViewSet):
@@ -10,3 +12,5 @@ class ProdutoViewSet(viewsets.ModelViewSet):
     serializer_class = ProdutosSerializer
     ordering_fields = ['titulo','preco']
     search_fields = ['titulo']
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
