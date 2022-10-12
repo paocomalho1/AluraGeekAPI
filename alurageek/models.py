98a8b8fdeaad75477819c475e8873e6873fcb8ef
ERICK <erick_honorio20@hotmail.com>
2022-10-12 17:32:53 -0300

    chave estrangeira no produto

--- alurageek/models.py
+++ alurageek/models.py
@@ -1,20 +1,5 @@
 from django.db import models
 
-class Produtos(models.Model):
-    CATEGORIA=(
-        ("SW","Star Wars"),
-        ("C","Console"),
-        ("D", "Diversos")    
-    )
-    titulo = models.CharField(max_length=30)
-    preco = models.FloatField(max_length=10)
-    categoria = models.CharField(max_length=3, choices=CATEGORIA, blank=False, null=False, default="D")
-    url = models.CharField(max_length=100)
-    descricao = models.TextField(max_length=500)
-
-    def __str__(self):
-        return self.titulo
-
 class Users(models.Model):
 
     nome = models.CharField(max_length=30)
@@ -25,3 +10,18 @@ class Users(models.Model):
     def __str__(self):
         return self.nome
 
+class Produtos(models.Model):
+    CATEGORIA=(
+        ("SW","Star Wars"),
+        ("C","Console"),
+        ("D", "Diversos")    
+    )
+    user = models.ForeignKey(Users ,on_delete=models.CASCADE, default=1)
+    titulo = models.CharField(max_length=30)
+    preco = models.FloatField(max_length=10)
+    categoria = models.CharField(max_length=3, choices=CATEGORIA, blank=False, null=False, default="D")
+    url = models.CharField(max_length=100)
+    descricao = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.titulo
