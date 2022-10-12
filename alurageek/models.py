b9bf89c88e63d0b35072e9ef89d0d93f3c83fc17
ERICK <erick_honorio20@hotmail.com>
2022-10-12 18:16:36 -0300

    fron-end

--- alurageek/models.py
+++ alurageek/models.py
@@ -20,8 +20,8 @@ class Produtos(models.Model):
     titulo = models.CharField(max_length=30)
     preco = models.FloatField(max_length=10)
     categoria = models.CharField(max_length=3, choices=CATEGORIA, blank=False, null=False, default="D")
-    url = models.CharField(max_length=100)
-    descricao = models.TextField(max_length=500)
+    url = models.CharField(max_length=500)
+    descricao = models.TextField(max_length=1200)
 
     def __str__(self):
         return self.titulo
